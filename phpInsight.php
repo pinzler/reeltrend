00ea6fce97e05451ca7acd1eccb65add8670b007
pinzler <pinzler@gmail.com>
2012-03-13 18:44:46 -0300

    Update phpInsight.php

--- phpInsight.php
+++ phpInsight.php
@@ -1,24 +1,5 @@
 <?php
-    /*
-     phpInsight is a Naive Bayes classifier to calculate sentiment. The program
-     uses a database of words categorised as positive, negative or neutral
-     
-     Copyright (C) 2011  James Hennessey
-     
-     This program is free software: you can redistribute it and/or modify
-     it under the terms of the GNU General Public License as published by
-     the Free Software Foundation, either version 3 of the License, or
-     (at your option) any later version.
-     
-     This program is distributed in the hope that it will be useful,
-     but WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     GNU General Public License for more details.
-     
-     You should have received a copy of the GNU General Public License
-     along with this program.  If not, see <http://www.gnu.org/licenses/>
-     
-     */
+  
     
     class Sentiment{
         private $ignoreList = array(); //list of token to ignore()
@@ -256,7 +237,7 @@
             return $wordList;
         }//Close getIgnoreList Function
         
-        public function _cleanString($string)  //Function to clean a string so all characters with accents are turned into ASCII characters. EG: ‡ = a
+        public function _cleanString($string)  //Function to clean a string so all characters with accents are turned into ASCII characters. EG: ï¿½ = a
         {
             $diac =
 			/* A */   chr(192).chr(193).chr(194).chr(195).chr(196).chr(197).
